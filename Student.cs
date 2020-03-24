3863617bb97a3c3a302395398b360900ebad88cc
srinivaschandana <31610522+srinivaschandana@users.noreply.github.com>
2020-03-24 09:58:49 +0530

    Add files via upload
    cmt

---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SampleProject.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StdId { get; set; }
+        public string StudentName { get; set; }
+        public string Branch { get; set; }
+        public int Fee { get; set; }
+
+    }
+}
